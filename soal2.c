e1a28725dab2906ac5fcd97426e454fb3458a53f
Akhdannnn <akhdanahmad09@gmail.com>
2026-05-11 09:06:29 +0700

    Refactor cycle detection function in graph

--- soal2.c
+++ soal2.c
@@ -1,34 +1,44 @@
+/** EL2008 Praktikum Pemecahan Masalah dengan Pemrograman 2024/2025
+*   Modul               : 5 – Foundation of Algorithm
+*   Hari dan Tanggal    : Senin, 11 Mei 2026
+*   Nama (NIM)          : Ahmad Akhdan Tristan Salman (13224039)
+*   Nama File           : soal2.c
+*   Deskripsi           : Program untuk memeriksa graph dependensi tidak memiliki siklus
+* 
+*/
+
 #include <stdio.h>
+#include <string.h>
 
 #define MAX 100
 
 // Fungsi DFS untuk mendeteksi siklus
-int dfsCycleCheck(int v, int graf[MAX][MAX], int visited[], int recStack[], int misi) {
+int cycleCheck(int v, int graf[MAX][MAX], int visited[], int stack[], int misi) {
     visited[v] = 1;
-    recStack[v] = 1;
+    stack[v] = 1;
 
     for (int i = 0; i < misi; i++) {
         if (graf[v][i]) {
-            if (!visited[i] && dfsCycleCheck(i, graf, visited, recStack, misi)) {
+            if (not visited[i] && cycleCheck(i, graf, visited, stack, misi)) {
                 return 1;
-            } else if (recStack[i]) {
+            } else if (stack[i]) {
                 return 1; 
             }
         }
     }
 
-    recStack[v] = 0;
+    stack[v] = 0;
     return 0;
 }
 
 // Fungsi untuk mengecek apakah graf memiliki siklus
 int hasCycle(int graf[MAX][MAX], int misi) {
     int visited[MAX] = {0};
-    int recStack[MAX] = {0};
+    int stack[MAX] = {0};
 
     for (int i = 0; i < misi; i++) {
-        if (!visited[i]) {
-            if (dfsCycleCheck(i, graf, visited, recStack, misi)) {
+        if (not visited[i]) {
+            if (cycleCheck(i, graf, visited, stack, misi)) {
                 return 1;
             }
         }
